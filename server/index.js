688f0289773c4591d87f362e8e86469388b9e06c
Md. Mehedi Hassan <howlader.mehedihassan@gmail.com>
2023-07-20 00:17:09 +0600

    chore: complete sockets channel and their actions

--- server/index.js
+++ server/index.js
@@ -1,24 +1,31 @@
+// Import Modules
 const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// Create Server
+const app = express();
+const http = require("http").createServer(app);
+
+// Middleware
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
+// Initialize Socket.io
 const io = require("socket.io")(http, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "https://ef66-114-130-81-43.ngrok-free.app"],
     },
 });
+
+// Declare constants
 const PORT = process.env.PORT || 5000;
 const socketList = {};
 
 app.get("/health-check", (req, res) => {
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true, message: "Server is running" });
 });
 
 app.get("/", (req, res) => {
@@ -28,26 +35,94 @@ app.get("/", (req, res) => {
 io.on("connection", socket => {
     console.log("A user connected with id: ", socket.id);
 
+    // User Disconnects from a room
     socket.on("disconnect", () => {
         socket.disconnect();
         console.log("user disconnected");
     });
 
-    socket.on("server-join-room", ({ roomId, userName }) => {
+    // User Joins a Room
+    socket.on("[SERVER]-JOIN-ROOM", ({ roomId, userName }) => {
         socket.join(roomId);
         socketList[socket.id] = { userName, video: true, audio: true };
 
-        io.sockets.in(roomId).clients((err, clients) => {
-            try {
-                const users = clients.map(client => {
-                    return { userId: client, information: socketList[client] };
-                });
-
-                socket.broadcast.to(roomId).emit("client-user-joined-room", users);
-            } catch (err) {
+        // Broadcast to all users in the room that a user has joined the room
+        io.sockets.in(roomId).clients((error, clients) => {
+            if (error) {
                 console.log(err);
+                io.sockets.in(roomId).emit("[SERVER]-ERROR-OCCURRED", { error: true });
             }
+
+            const users = clients.map(client => {
+                return { userId: client, information: socketList[client] };
+            });
+
+            socket.broadcast.to(roomId).emit("[CLIENT]-USER-Joined", users);
         });
+    });
+
+    // Check if the user is already in the room
+    socket.on("[SERVER]-CHECK-USER", ({ roomId, userName }) => {
+        let userExists = false;
+
+        // Match user name with the current users in the room
+        io.sockets.in(roomId).clients((error, clients) => {
+            if (error) {
+                console.log(error);
+                io.sockets.in(roomId).emit("[SERVER]-ERROR-OCCURRED", { error: true });
+            }
+
+            clients.forEach(client => {
+                if (socketList[client].userName === userName) {
+                    userExists = true;
+                }
+            });
+
+            socket.emit("[SERVER]-USER-EXISTS", userExists);
+        });
+    });
+
+    // Call user
+    socket.on("[SERVER]-CALL-USER", ({ recieverInformation, from, signal }) => {
+        io.to(recieverInformation).emit("[CLIENT]-RECIEVE-CALL", {
+            signal,
+            from,
+            information: socketList[socket.id],
+        });
+    });
+
+    // Recieve call
+    socket.on("[SERVER]-ACCEPT-CALL", ({ signal, to }) => {
+        io.to(to).emit("[CLIENT]-CALL-ACCEPTED", {
+            signal,
+            information: socketList[socket.id],
+        });
+    });
+
+    // Send Message
+    socket.on("[SERVER]-SEND-MESSAGE", ({ roomId, message, sender }) => {
+        io.sockets.in(roomId).emit("[CLIENT]-RECEIVE-MESSAGE", { message, sender });
+    });
+
+    // Leave Room
+    socket.on("[SERVER]-LEAVE-ROOM", ({ roomId, userName }) => {
+        delete socketList[socket.id];
+
+        socket.broadcast.to(roomId).emit("[CLIENT]-USER-Left", { userId: socket.id, userName });
+
+        io.sockets.sockets[socket.id].leave(roomId);
+    });
+
+    // Toggle Video Audio
+    socket.on("[SERVER]-TOGGLE-VIDEO-AUDIO", ({ roomId, video, audio }) => {
+        if (video) {
+            socketList[socket.id].video = video;
+        }
+        if (audio) {
+            socketList[socket.id].audio = audio;
+        }
+
+        socket.broadcast.to(roomId).emit("[CLIENT]-TOGGLE-VIDEO-AUDIO", { userId: socket.id, video, audio });
     });
 });
 
