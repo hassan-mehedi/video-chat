a7d8da2c99c3f263a76ed560e156babb77201c1e
Md. Mehedi Hassan <howlader.mehedihassan@gmail.com>
2023-07-20 00:36:18 +0600

    fix: convert all channel name uppercase

--- server/index.js
+++ server/index.js
@@ -50,14 +50,14 @@ io.on("connection", socket => {
         io.sockets.in(roomId).clients((error, clients) => {
             if (error) {
                 console.log(err);
-                io.sockets.in(roomId).emit("[SERVER]-ERROR-OCCURRED", { error: true });
+                io.sockets.in(roomId).emit("[SERVER]-ERROR-OCCURRED-USER-EXISTS", { error: true });
             }
 
             const users = clients.map(client => {
                 return { userId: client, information: socketList[client] };
             });
 
-            socket.broadcast.to(roomId).emit("[CLIENT]-USER-Joined", users);
+            socket.broadcast.to(roomId).emit("[CLIENT]-USER-JOINED", users);
         });
     });
 
@@ -69,7 +69,7 @@ io.on("connection", socket => {
         io.sockets.in(roomId).clients((error, clients) => {
             if (error) {
                 console.log(error);
-                io.sockets.in(roomId).emit("[SERVER]-ERROR-OCCURRED", { error: true });
+                io.sockets.in(roomId).emit("[SERVER]-ERROR-OCCURRED-USER-EXISTS", { error: true });
             }
 
             clients.forEach(client => {
@@ -108,7 +108,7 @@ io.on("connection", socket => {
     socket.on("[SERVER]-LEAVE-ROOM", ({ roomId, userName }) => {
         delete socketList[socket.id];
 
-        socket.broadcast.to(roomId).emit("[CLIENT]-USER-Left", { userId: socket.id, userName });
+        socket.broadcast.to(roomId).emit("[CLIENT]-USER-LEFT", { userId: socket.id, userName });
 
         io.sockets.sockets[socket.id].leave(roomId);
     });
