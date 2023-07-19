5c7a9d780c09e82936b8f7dcf722b18e2435ed50
Md. Mehedi Hassan <howlader.mehedihassan@gmail.com>
2023-07-20 00:35:52 +0600

    chore: add all the components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,34 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 
-import Home from "./pages/Home";
-import Room from "./pages/Room";
+import Main from "./components/Main";
+import Room from "./components/Room";
 
 import "./App.css";
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/room/:roomId" element={<Room />} />
-            </Routes>
+            <AppContainer>
+                <Routes>
+                    <Route exact path="/" element={<Main />} />
+                    <Route exact path="/room/:roomId" element={<Room />} />
+                </Routes>
+            </AppContainer>
         </BrowserRouter>
     );
 }
+
+const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(8px + 2vmin);
+    color: white;
+    background-color: #454552;
+    text-align: center;
+`;
 
 export default App;
